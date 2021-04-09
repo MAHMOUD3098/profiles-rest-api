5ab9afbfec49ecc99f25950a2db6441b504a81aa
MAHMOUD SALEM <48593084+MAHMOUD3098@users.noreply.github.com>
2021-04-09 10:03:46 +0200

    Added our first viewset and router

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
 
 from . import serializers
 # Create your views here.
@@ -33,23 +34,37 @@ class HelloApiView(APIView):
         if serializer.is_valid():
             name = serializer.data.get('name')
             message = 'Hello {0} '.format(name)
-            return Response({'message':message})
+            return Response({'message': message})
 
         else:
-            return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
-
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def put(self, request, pk=None):
         """Handles updating an object."""
 
-        return Response({'method':'put'})
+        return Response({'method': 'put'})
 
     def patch(self, request, pk=None):
         """Patch request, only update fields provided in the request."""
 
-        return Response({'method':'patch'})
+        return Response({'method': 'patch'})
 
     def delete(self, request, pk=None):
         """Deletes an object."""
 
         return Response({'method': 'delete'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test API ViewSet."""
+
+    def list(self, request):
+        """Returns a hello message."""
+
+        a_viewset = [
+            'Uses actions (list, create, retrieve, update, partial_update)'
+            'Automatically maps to URLS using Routers',
+            'Provides more functionality with less code'
+        ]
+
+        return Response({'message':'Hello!', 'a_viewset':a_viewset})
